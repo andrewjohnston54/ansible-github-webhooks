a5fc45058f720d3c78ecaa3d80a8f1260463ead1
Steven Harradine <stevenharradine@gmail.com>
2016-03-05 16:55:06 -0500

    iterate over events

--- templates/config.js
+++ templates/config.js
@@ -1,5 +1,5 @@
 module.exports.GITHUB_TOKEN = "{{ githubwebhooks_github_token }}"
 module.exports.GITHUB_ORG = "{{ githubwebhooks_github_organization }}"
 module.exports.WEB_HOOK = "{{ githubwebhooks_web_hook }}"
-module.exports.GITHUB_EVENTS = {{ githubwebhooks_github_events }}
+module.exports.GITHUB_EVENTS = [ {% for event in githubwebhooks_github_events %}["{{ event }}"{% if not loop.last %},{% endif %}]{% endfor %}
 module.exports.GITHUB_REPO_FILTER = process.argv[2]
